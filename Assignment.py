1f34a73127d90f703ad2f1b27144757eac8a4441
Ganainmtech <127313669+Ganainmtech@users.noreply.github.com>
2023-11-27 21:20:47 +0000

    reading each lang txt file

--- Assignment.py
+++ Assignment.py
@@ -3,13 +3,15 @@
 
 import random
 import datetime
+from typing import TextIO
 
 first_name = input("Enter your first name? ")
 surname_name = input("Enter your surname? ")
 
 score = 0
 result = 0
-feedback = ""
+feedback_lang = ""
+feedback_math = ""
 q_asked = 1
 percentage = 0
 user_finished = 'no'
@@ -39,20 +41,20 @@ while user_finished == 'no':
                 print(f"{q_asked}:", random_num_one, "-", random_num_two, "= ", end="")
                 rand_math_ans = random_num_one - random_num_two
                 user_ans = int(input(""))
-                feedback += f"\n{q_asked}: {random_num_one} - {random_num_two} = {user_ans}"
+                feedback_math += f"\n{q_asked}: {random_num_one} - {random_num_two} = {user_ans}"
             else:
                 # else just add the rand nums together
                 print(f"{q_asked}:", random_num_one, "+", random_num_two, "= ", end="")
                 rand_math_ans = random_num_one + random_num_two
                 user_ans = int(input(""))
-                feedback += f"\n{q_asked}: {random_num_one} + {random_num_two} = {user_ans}"
+                feedback_math += f"\n{q_asked}: {random_num_one} + {random_num_two} = {user_ans}"
 
             # Add to score and finished the string output
             if rand_math_ans == user_ans:
                 score += 1
-                feedback += f" \u2705"
+                feedback_math += f" \u2705"
             else:
-                feedback += f" \u274C should be {rand_math_ans}"
+                feedback_math += f" \u274C should be {rand_math_ans}"
 
             # Calculate the score percentage and add 1 to questions asked count
             percentage = float(score / q_asked * 100)
@@ -62,44 +64,58 @@ while user_finished == 'no':
             if q_asked > num_math_q:
                 print("\nFeedback:")
                 if rand_math_ans == user_ans:
-                    print(f"{feedback}")
+                    print(f"{feedback_math}")
                 else:
-                    print(f"{feedback}")
+                    print(f"{feedback_math}")
         print(f"\n{first_name}, you got {percentage:,.0f}% \U0001F389")
 
-    #user_finished = input(f"Are you finished? (yes/no)")
-    #print("Goodbye!")
+    # user_finished = input(f"Are you finished? (yes/no)")
+    # print("Goodbye!")
 
     elif user_choice == 2:
-        connection = open("polish.txt", "r")
+
+        user_level = int(input(f"What is your current level {first_name}?"))
+
+        if user_level == 1:
+            connection = open("Polish1.txt", "r")
+        elif user_level == 2:
+            connection = open("Polish2.txt", "r")
+        elif user_level == 3:
+            connection = open("Polish3.txt", "r")
+        elif user_level == 4:
+            connection = open("Polish4.txt", "r")
+        else:
+            connection = open("Polish5.txt", "r")
 
         for word in connection:
             word = word.rstrip()
             word = word.split(",")
             english_word = word[0]
             polish_word = word[1]
+            print(user_level)
 
             print(f"{english_word} = ")
             user_word = input("")
 
-            feedback += f"\n{english_word} = {user_word}"
+            feedback_lang += f"\n{english_word} = {user_word}"
 
             # Add to score and finished the string output
             if user_word == polish_word:
                 score += 1
-                feedback += f" \u2705"
+                feedback_lang += f" \u2705"
             else:
-                feedback += f" \u274C should be {polish_word}"
+                feedback_lang += f" \u274C should be {polish_word}"
 
             percentage = float(score / 3 * 100)
 
         # If the questions are finished print the feedback from the quiz
         print("\nFeedback:")
         if user_word == polish_word:
-            print(f"{feedback}")
+            print(f"{feedback_lang}")
         else:
-            print(f"{feedback}")
+            print(f"{feedback_lang}")
 
         print(f"\nResult: {score}/3\n{first_name}, you got {percentage:,.0f}% \U0001F389")
+        if percentage > 70:
+            print(f"The next time you use this program you should start with level 2")
         connection.close()
-
